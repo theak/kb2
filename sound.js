2d5c62371485b58c9afe9f7e1ae67b2c35b449b2
Akshay K <ak@aks.zone>
2018-08-02 01:03:58 -0700

    added radio buuttons

--- sound.js
+++ sound.js
@@ -1,8 +1,29 @@
+const $ = (q) => document.querySelectorAll(q);
+
 function handleKeyDown(e) {
   var key = e.key;
-  if (key === "Backspace" || key === " ") {
-  	console.log(2);
+  if (key === "Backspace") {
+    console.log(3);
+  } else if (key === " ") {
+    console.log(2);
   } else if (key.length === 1) {
-  	console.log(1);
+    console.log(1);
   }
 }
+
+var sounds = ["1.wav", "2.wav", "3.wav"];
+var soundsets = ["mainsounds", "spacesounds", "backspacesounds"];
+
+document.addEventListener("DOMContentLoaded", () => { 
+  soundsets.forEach((soundSet, soundSetIndex) => {
+    var set = document.getElementById(soundSet);
+    sounds.forEach((sound, soundIndex) => {
+      var tmpl = document.getElementById("radio").content.cloneNode(true);
+      tmpl.querySelector("input").id = soundSetIndex + "_" + soundIndex;
+      tmpl.querySelector("input").name = soundSetIndex;
+      tmpl.querySelector("label").htmlFor = soundSetIndex + "_" + soundIndex;
+      tmpl.querySelector("label").textContent = sound;
+      set.appendChild(tmpl);
+    });
+  });
+});
