e029a2a0962a6bada1abcafcdbcc2a5fd788429b
Akshay Kannan <akshayk@akshayk-macbookpro3.roam.corp.google.com>
2018-08-02 11:12:39 -0700

    added ability to change sounds

--- sound.js
+++ sound.js
@@ -1,26 +1,39 @@
 const $ = (q) => document.querySelectorAll(q);
 
+const getSound = (id) => document.getElementById("sound" + id);
 function handleKeyDown(e) {
   var key = e.key;
   if (key === "Backspace") {
-    console.log(3);
+    getSound(2).play();
   } else if (key === " ") {
-    console.log(2);
+    getSound(1).play();
   } else if (key.length === 1) {
-    console.log(1);
+    getSound(0).play();
   }
 }
 
-var sounds = ["1.wav", "2.wav", "3.wav"];
+var sounds = ["1", "1_4800", "1_2800", "1_1600", "1_1200", "1_1000", "1_800", 
+  "2", "2_4800"];
 var soundsets = ["mainsounds", "spacesounds", "backspacesounds"];
+
+const getWav = (sound) => "wav/" + sound + ".wav";
 
 document.addEventListener("DOMContentLoaded", () => { 
   soundsets.forEach((soundSet, soundSetIndex) => {
     var set = document.getElementById(soundSet);
     sounds.forEach((sound, soundIndex) => {
       var tmpl = document.getElementById("radio").content.cloneNode(true);
+
       tmpl.querySelector("input").id = soundSetIndex + "_" + soundIndex;
       tmpl.querySelector("input").name = soundSetIndex;
+      if (getSound(soundSetIndex).firstChild.src.endsWith(getWav(sound))) {
+        tmpl.querySelector("input").checked = true;
+      }
+      tmpl.querySelector("input").onchange = () => {
+        console.log(getWav(sound));
+        getSound(soundSetIndex).firstChild.src = getWav(sound);
+        getSound(soundSetIndex).load();
+      };
       tmpl.querySelector("label").htmlFor = soundSetIndex + "_" + soundIndex;
       tmpl.querySelector("label").textContent = sound;
       set.appendChild(tmpl);
---
+++ upload.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+export AWS_ACCESS_KEY_ID='AKIAJUZ6GGMCUX3PQXXA'
+export AWS_SECRET_ACCESS_KEY='wZ19YCOAAMyw0Auvixb+Obu3btasDWMVR8QjEm4D'
+
+mkdir -p bin
+if ! type "aws" > /dev/null 2> /dev/null; then
+  echo "installing awscli- please enter your sudo password:"
+  sudo pip install awscli
+fi
+
+# Remove useless files
+#find . -name ".DS_Store" | xargs rm
+#find . -name "*~" | xargs rm
+
+aws s3 cp . s3://kb-sound/ --recursive --exclude *~ --exclude ".git*" --dryrun
